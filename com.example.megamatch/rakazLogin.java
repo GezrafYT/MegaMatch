a7db0914775f212055d703e1d74b6f7dc19aea96
Gezraf <59087430+GezrafYT@users.noreply.github.com>
2025-02-23 10:30:14 +0200

    Update and rename MainActivity3.java to rakazLogin.java

--- com.example.megamatch/rakazLogin.java
+++ com.example.megamatch/rakazLogin.java
@@ -3,18 +3,18 @@ package com.example.megamatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 
-public class MainActivity3 extends AppCompatActivity {
+public class rakazLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_three);
+        setContentView(R.layout.rakaz_login);
+
+
+        final View layout = findViewById(R.id.rakazLoginLayout);
 
     }
-
-
-
-
 }
