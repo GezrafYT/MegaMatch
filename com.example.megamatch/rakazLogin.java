bf7b5fabf7923da6f0cd6873b4900e4ebb336330
Gezraf <59087430+GezrafYT@users.noreply.github.com>
2025-02-23 11:40:50 +0200

    Update rakazLogin.java

--- com.example.megamatch/rakazLogin.java
+++ com.example.megamatch/rakazLogin.java
@@ -1,20 +1,34 @@
 package com.example.megamatch;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class rakazLogin extends AppCompatActivity {
+
+    private EditText schoolIdInput, schoolNameInput, usernameInput, passwordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rakaz_login);
 
+        schoolIdInput = findViewById(R.id.schoolIDInput);
+        schoolNameInput = findViewById(R.id.schoolNameInput);
+        usernameInput = findViewById(R.id.usernameInput);
+        passwordInput = findViewById(R.id.passwordInput);
+    }
 
-        final View layout = findViewById(R.id.rakazLoginLayout);
+    public void registerRakaz(View view) {
+        Toast.makeText(this, "Registration button clicked", Toast.LENGTH_SHORT).show();
+    }
 
+    public void moveToRakazRegister(View view)
+    {
+        Intent i1 = new Intent(this, rakazRegister.class);
+        startActivity(i1);
     }
 }
