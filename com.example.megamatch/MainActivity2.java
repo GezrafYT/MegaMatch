b3914ddfecbd18b72ca7b936936dc6992f8369d4
Gezraf <59087430+GezrafYT@users.noreply.github.com>
2025-02-23 10:04:45 +0200

    Add files via upload

---
+++ com.example.megamatch/MainActivity2.java
@@ -0,0 +1,23 @@
+package com.example.megamatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+
+        final View layout = findViewById(R.id.mainLayout2);
+
+    }
+
+
+
+
+}
