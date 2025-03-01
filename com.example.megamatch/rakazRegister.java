687f4e9ebc99349a56e0a0d0a597eb4671e0de87
Gezraf <59087430+GezrafYT@users.noreply.github.com>
2025-03-02 01:08:36 +0200

    Update rakazRegister.java

--- com.example.megamatch/rakazRegister.java
+++ com.example.megamatch/rakazRegister.java
@@ -1,8 +1,10 @@
 package com.example.megamatch;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,12 +19,16 @@ public class rakazRegister extends AppCompatActivity {
         String[] megamaList = {
                 "מחשבים", "כימיה", "ביולוגיה", "פיסיקה", "ערבית",
                 "מכטרוניקה", "רובוטיקה", "טלוויזיה", "אומנות",
-                "משפטים", "תיאטרון", "משפט עברי"
+                "משפטים", "תיאטרון", "מחול", "משפט עברי", "אלטקטרוניקה"
         };
 
         // Initialize the Spinner
         Spinner megamaSpinner = findViewById(R.id.megamaSpinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, megamaList);
         megamaSpinner.setAdapter(adapter);
+    }
+
+    public void registerRakaz(View view) {
+        Toast.makeText(this, "Registration button clicked", Toast.LENGTH_SHORT).show();
     }
 }
