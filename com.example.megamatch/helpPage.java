216dae412d763f8b6e83db7dda70fbdaa9e0ae7e
Gezraf <59087430+GezrafYT@users.noreply.github.com>
2025-03-09 11:42:16 +0200

    Update helpPage.java

--- com.example.megamatch/helpPage.java
+++ com.example.megamatch/helpPage.java
@@ -1,24 +1,57 @@
 package com.example.megamatch;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.megamatch.PageFragment;
 
 public class helpPage extends AppCompatActivity {
+
+    private Button page1Button, page2Button, page3Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.help_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.helpPageLayout), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Initialize buttons
+        page1Button = findViewById(R.id.page1Button);
+        page2Button = findViewById(R.id.page2Button);
+        page3Button = findViewById(R.id.page3Button);
+
+        // Set default fragment (Page 1)
+        replaceFragment(PageFragment.newInstance("Page 1"));
+
+        // Set button click listeners
+        page1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(PageFragment.newInstance("Page 1"));
+            }
         });
+
+        page2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(PageFragment.newInstance("Page 2"));
+            }
+        });
+
+        page3Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(PageFragment.newInstance("Page 3"));
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.helpFragmentContainer, fragment);
+        transaction.commit();
     }
 }
